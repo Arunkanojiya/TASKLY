21af2a9903eb6fb78e9648256758f27feef4db54
ArunKanojiya <arunkanojiya123@gmail.com>
2025-10-14 20:09:19 +0530

    changes in .env

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import adminRoutes from './routes/admin.routes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2000;
 ;
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
